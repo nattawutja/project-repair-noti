e6ec0086a829103e6d333a72cc41160718eb4f1b
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-12 13:47:56 +0700

    fix query status work fix wording status 1 and add status 2

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json");
 require_once 'db.php';
 $data = [];
 // ทดสอบ query ง่าย ๆ
-$qry = 'select CASE WHEN t1."StatusWork" = 0 THEN \'รอ IT ตรวจสอบ\' else \'ดำเนินการแล้ว\' end as status, CASE WHEN t1."SystemType" = \'P\' THEN \'P/C\' else \'AS/400\' end as systemname,t2."name_Device",to_char(t1."create_date",\'DD/MM/YYYY HH24:MI:SS\')as cvcreatedate,t1.* from "rp_Repair_Notify" t1
+$qry = 'select CASE WHEN t1."StatusWork" = 0 THEN \'รอ IT ตรวจสอบ\' WHEN t1."StatusWork" = 1 THEN \'รอผู้แจ้งตรวจสอบ\' else \'จบงาน\' end as status, CASE WHEN t1."SystemType" = \'P\' THEN \'P/C\' else \'AS/400\' end as systemname,t2."name_Device",to_char(t1."create_date",\'DD/MM/YYYY HH24:MI:SS\')as cvcreatedate,t1.* from "rp_Repair_Notify" t1
 left join "Master_Device_Type" t2 on t1."DeviceTypeID" = t2."id" and t2."StatusDelete" = 0
 where t1."StatusDelete" = 0 ORDER BY t1."RepairID" DESC limit 15 offset 0';
 $result = pg_query($Con, $qry);
