510ccb8d862ef2eda64e929782332c0d1a3079b5
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-18 08:25:47 +0700

    get employeeIT array_agg

---
+++ backend/getDataEmpIT.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+require 'db.php';
+$data = [];
+$qryEmpIT = 'WITH cte as (SELECT array_agg(concat("firstName",\' \',"lastName")) AS names FROM "User"
+WHERE "role" = \'USER\'
+GROUP BY "role")
+SELECT * from cte';
+
+$res = pg_query($Con,$qryEmpIT);
+if(pg_num_rows($res) > 0){
+ while($dt = pg_fetch_assoc($res)){
+    $substrname = str_replace(['{','}'],'',$dt["names"]);
+    $substrname = str_replace('"', '', $substrname); 
+    $data[] = $substrname;
+ }
+}
+
+echo json_encode([
+    'success' => true,
+    'data' => $data
+]);
+
+
+pg_close($Con);
+
+?>
