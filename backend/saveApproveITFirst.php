1862190908024f7b0acb7c9c424a8367e635c28a
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-25 15:35:02 +0700

    add field you userid keep in tbl main

--- backend/saveApproveITFirst.php
+++ backend/saveApproveITFirst.php
@@ -8,24 +8,27 @@ $RepairID = intval($_GET['idrp']); // แปลงเป็นเลขจำน
 $description = $_GET['description'];
 $fullname = $_GET['fullname'];
 $user_id = $_GET['user_id'];
+$process = $_GET['process'];
 
-$qryApprove = 'INSERT INTO "rp_Repair_Notify_Approve" ("RepairID","fullName","Approve","pos_id","user_id","description") VALUES (';
+$qryApprove = 'INSERT INTO "rp_Repair_Notify_Approve" ("RepairID","fullName","Approve","pos_id","user_id","description","ProcessDate") VALUES (';
 $qryApprove .= '' . $RepairID . ',';
 $qryApprove .= '\'' . $fullname . '\',';
 $qryApprove .= '\'' . "O" . '\',';
 $qryApprove .= '' . 0 . ',';
 $qryApprove .= '\'' . $user_id . '\',';
 $qryApprove .= '\'' . $description . '\'';
+$qryApprove .= '\'' . $date -> format('Y-m-d H:i:s') . '\'';
 $qryApprove .= ');';
-pg_query($Con, $qryApprove);
+//pg_query($Con, $qryApprove);
 
-$qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 1 . ' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' ';
-pg_query($Con, $qryUptTableMain);
+$qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 1 . ' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' AND "user_id_IT" = \'' . $user_id . '\'';
+//pg_query($Con, $qryUptTableMain);
 
 echo json_encode([
     'success' => true,
     'qry' => $qryApprove,
-    'qryUptMain' => $qryUptTableMain
+    'qryUptMain' => $qryUptTableMain,
+    'Status' => $process
 ]);
 
 pg_close($Con);
