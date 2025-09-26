2b3cda2dc166af30068e69d9aab1db0a8fe54b4e
Nattawut.ch <b.nattawut79@gmail.com>
2025-09-26 13:30:22 +0700

    fix qry send work

--- backend/saveApproveSendWork.php
+++ backend/saveApproveSendWork.php
@@ -7,15 +7,36 @@ $date = new DateTime("now", new DateTimeZone('Asia/Bangkok'));
 $ApproveID = intval($_GET['id']); // แปลงเป็นเลขจำนวนเต็ม
 $RepairID = intval($_GET['idrp']); // แปลงเป็นเลขจำนวนเต็ม
 $posid = intval($_GET['posid']); // แปลงเป็นเลขจำนวนเต็ม
+$process = $_GET['process'];
 
-$qryApprove = 'UPDATE "rp_Repair_Notify_Approve" SET "Approve" = \'' . "Y" . '\',"ApproveDate" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "ApproveID" = ' . $ApproveID . ' AND "RepairID" = ' . $RepairID . ' AND "pos_id" = ' . $posid . ' ';
+if($process == "Y"){
+    $qryApprove = 'UPDATE "rp_Repair_Notify_Approve" SET "Approve" = \'' . "Y" . '\',"ApproveDate" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "ApproveID" = ' . $ApproveID . ' AND "RepairID" = ' . $RepairID . ' AND "pos_id" = ' . $posid . ' ';
+}else if($process == "W"){
+    $qryApprove = 'UPDATE "rp_Repair_Notify_Approve" SET "Approve" = \'' . "W" . '\',"ProcessDate" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "ApproveID" = ' . $ApproveID . ' AND "RepairID" = ' . $RepairID . ' AND "pos_id" = ' . $posid . ' ';
+}else if($process == "S"){
+    $qryApprove = 'UPDATE "rp_Repair_Notify_Approve" SET "Approve" = \'' . "S" . '\',"ProcessDate" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "ApproveID" = ' . $ApproveID . ' AND "RepairID" = ' . $RepairID . ' AND "pos_id" = ' . $posid . ' ';
+}else{
+    $qryApprove = 'UPDATE "rp_Repair_Notify_Approve" SET "Approve" = \'' . "O" . '\',"ProcessDate" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "ApproveID" = ' . $ApproveID . ' AND "RepairID" = ' . $RepairID . ' AND "pos_id" = ' . $posid . ' ';
+}
+
+if($process == "Y"){
+    $qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 4 . ',"send_work_date" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' ';
+}else if($process == "W"){
+    $qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 3 . ',"send_work_date" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' ';
+}else if($process == "S"){
+    $qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 2 . ',"send_work_date" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' ';
+}else{
+    $qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 1 . ',"send_work_date" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' ';
+}
+
 pg_query($Con, $qryApprove);
-
-$qryUptTableMain = 'UPDATE "rp_Repair_Notify" SET "StatusWork" = ' . 2 . ',"send_work_date" = \'' . $date -> format('Y-m-d H:i:s') . '\' where "StatusDelete" = 0 AND "RepairID" = ' . $RepairID . ' ';
 pg_query($Con, $qryUptTableMain);
 
 echo json_encode([
-    'success' => true
+    'success' => true,
+    'qryApprove' => $qryApprove,
+    'process' => $process,
+    'qryUptTableMain' => $qryUptTableMain
 ]);
 
 
