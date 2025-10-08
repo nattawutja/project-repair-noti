c652fb8e581190177476df1f7c50c14ea1632639
nattawut.ch <b.nattawut79@gmail.com>
2025-10-08 14:00:09 +0700

    fixed where and group

--- backend/getDataEmpIT.php
+++ backend/getDataEmpIT.php
@@ -4,8 +4,8 @@ header("Content-Type: application/json");
 require 'db.php';
 $data = [];
 $qryEmpIT = 'WITH cte as (SELECT array_agg(concat("firstName",\' \',"lastName")) AS names FROM "User"
-WHERE "role" = \'USER\'
-GROUP BY "role")
+WHERE "position" = \'เจ้าหน้าที่\' and "department" = \'MIS\'
+GROUP BY "position")
 SELECT * from cte';
 
 $res = pg_query($Con,$qryEmpIT);
