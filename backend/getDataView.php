640b4abd59a73c507dc90cc4630df2ad1a84b623
nattawut.ch <b.nattawut79@gmail.com>
2025-10-31 16:07:35 +0700

    modify qry get Data View

--- backend/getDataView.php
+++ backend/getDataView.php
@@ -8,11 +8,11 @@ header("Content-Type: application/json");
 require_once 'db.php';
 $repairID = $_GET["id"]; // ✅ ตรงนี้คือแก้ให้รับแบบ GET
 $data = [];
-$qry = 'select t1."StatusWork",t1."OtherTool",t1."DeviceTypeID",t1."SystemType",t1."RepairNotifyDate",t4."name" as dviname,t3."name",t1."RepairID",to_char(t1."create_date",\'DD/MM/YYYY HH24:MI:ss\') as cvcreatedate,t1."RepairNo",to_char(t1."RepairNotifyDate",\'DD/MM/YYYY\') as cvdate,t1."DptCode",t1."EmpName",CASE WHEN t1."SystemType" = \'P\' THEN \'P/C\' else \'AS/400\' end as systemtype,t2."name_Device",t1."DeviceToolID",t1."Model",t1."ToolAssetID",t1."description" from "rp_Repair_Notify" t1 
+$qry = 'with cte as (select t1."StatusWork",t1."OtherTool",t1."DeviceTypeID",t1."SystemType",t1."RepairNotifyDate",t4."name" as dviname,t3."name",t1."RepairID",to_char(t1."create_date",\'DD/MM/YYYY HH24:MI:ss\') as cvcreatedate,t1."RepairNo",to_char(t1."RepairNotifyDate",\'DD/MM/YYYY\') as cvdate,t1."DptCode",t1."EmpName",CASE WHEN t1."SystemType" = \'P\' THEN \'P/C\' else \'AS/400\' end as systemtype,t2."name_Device",t1."DeviceToolID",t1."Model",t1."ToolAssetID",t1."description" from "rp_Repair_Notify" t1 
 left join "Master_Device_Type" t2 on t1."DeviceTypeID" = t2."id"
 left join "Department" t3 on t1."DptCode" = t3."code"
 left join "Division" t4 on t1."DviCode" = t4."code"
-where t1."StatusDelete" = 0 and t1."RepairID" = ' . $repairID . ' ';
+where t1."StatusDelete" = 0 and t1."RepairID" = ' . $repairID . ' ) SELECT * FROM cte';
 $result = pg_query($Con, $qry);
 if ($result) {
   while ($row = pg_fetch_assoc($result)) {
